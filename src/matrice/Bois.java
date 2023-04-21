3331d82ef292e7cd5adebfb33a08b73569d0a635
ajorand <antoine.jorand@outlook.com>
2023-04-21 11:40:30 +0200

    test

--- src/matrice/Bois.java
+++ src/matrice/Bois.java
@@ -16,17 +16,40 @@ public class Bois extends Ressource implements Utilisable {
 	   ObjetManufacture de type Feu.
 	 * */
 	public ObjetManufacture utiliser(Ressource[] r) {
+		int nbrDeBois = 0;
 		
 		// Vérification que le joueur a 5 bois dans son inventaire
 		for(int i = 0; i < 6; i++) {
-			if(r[i] instanceof Bois) {
-				
+			if(r[i] != null) {
+				nbrDeBois++;
 			}
 		}
 		
+		// S'il en a au moins 5
+		if(nbrDeBois >= 5) {
+			
+			// On parcours le tableau pour retirer les 5 derniers bois
+			for(int i = 5; i == 0; i--) {
+				
+				if(nbrDeBois == nbrDeBois - 5) {
+					break;
+				}
+				
+				if(r[i] != null) {
+					r[i] = null;
+				}
+			}
+			
+			// Return
+			ObjetManufacture feu = new ObjetManufacture("Feu");
+			return feu;
+		}
+		else {
+			System.out.println("Vous n'avez pas assez de bois pour faire du feu");
+			return null;
+		}
 		
-		ObjetManufacture feu = new ObjetManufacture("Feu");
-		return feu;
+		
 	}
 	
 }
--- src/matrice/Pierre.java
+++ src/matrice/Pierre.java
@@ -9,4 +9,43 @@ private int poids = 3;
 		super(id);
 	}
 	
+	// Methods
+	
+	public ObjetManufacture utiliser(Ressource[] r) {
+		int nbrDeBle = 0;
+		
+		// Vérification que le joueur a 10 blés dans son inventaire
+		for(int i = 0; i < 13; i++) {
+			if(r[i] != null) {
+				nbrDeBle++;
+			}
+		}
+		
+		// S'il en a au moins 5
+		if(nbrDeBle >= 10) {
+			
+			// On parcours le tableau pour retirer les 10 derniers blés
+			for(int i = 12; i == 0; i--) {
+				
+				if(nbrDeBle == nbrDeBle - 10) {
+					break;
+				}
+				
+				if(r[i] != null) {
+					r[i] = null;
+				}
+			}
+			
+			// Return
+			ObjetManufacture feu = new ObjetManufacture("Farine");
+			return feu;
+		}
+		else {
+			System.out.println("Vous n'avez pas assez de blé pour faire de la farine");
+			return null;
+		}
+		
+		
+	}
+	
 }
